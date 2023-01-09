05639837abdd088df49a3c8f415568cc925b9253
brachtco <brachtdanny@gmail.com>
2023-01-09 13:32:04 -0700

    Add questions for manager engineer intern index.js. Begin HTMl and CSS. Add package.json and modules

--- index.js
+++ index.js
@@ -1,42 +1,94 @@
+//packages
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 const generateHTML = require("./generateHTML");
 
 
-//Input of Employee information
 const questions = [
+    {
+        type: "list",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern", "Finished employee input"],
+    }
+];
+
+const manager = [
     {
         type: "input",
         name: "name",
-        message: "What is your name?",
+        message: "Please enter the name of the Manager.",
     },
     {
         type: "input",
         name: "id",
-        message: "What is your employee id?",
+        message: "Please enter the ID of the Manager.",
     },
     {
         type: "input",
         name: "email",
-        message: "What is your email address?",
+        message: "Please enter the email address of the Manager.",
     },
-
-    //Addtional input of Manager
     {
         type: "input",
         name: "officeNumber",
-        message: "What is your office number?",
-    },
-    //Additonal input of Engineer
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?",
-    },
-    //Additional input of Intern
-    {
-        type: "input",
-        name: "school",
-        message: "Where do you go to school?"
+        message: "Please enter the office number of the Manager.",
     },
 ];
+
+const enginner = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter the name of the Eningeer.",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter the ID of the Eningeer.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter the email address of the Eningeer.",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the GitHub username of the Eningeer.",
+    },
+];
+
+const intern = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter the name of the Intern.",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter the ID of the Intern.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter the email address of the Intern.",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the name of the school that Intern currently attends.",
+    },
+];
+
+
+function init() {
+    inquirer.prompt(questions)
+    .then(inquirerResopnses) => {
+        console.log('Success';)
+    }
+}
